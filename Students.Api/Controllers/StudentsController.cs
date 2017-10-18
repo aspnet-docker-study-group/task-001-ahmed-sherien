3ae634a6e9f182b9b0fa84d8b09538486f2958a5
Ahmed Sherien <eng.a.sherien@gmail.com>
2017-10-18 22:24:23 +0200

    Adding StudentsController (scaffolded)

---
+++ Students.Api/Controllers/StudentsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Students.Api.Infrastructure;
+using Students.Api.Models;
+
+namespace Students.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Students")]
+    public class StudentsController : Controller
+    {
+        private readonly StudentsContext _context;
+
+        public StudentsController(StudentsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Students
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return _context.Students;
+        }
+
+        // GET: api/Students/{guid}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudent([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var student = await _context.Students.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // PUT: api/Students/{guid}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent([FromRoute] Guid id, [FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Students
+        [HttpPost]
+        public async Task<IActionResult> PostStudent([FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        }
+
+        // DELETE: api/Students/{guid}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var student = await _context.Students.SingleOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return Ok(student);
+        }
+
+        private bool StudentExists(Guid id)
+        {
+            return _context.Students.Any(e => e.Id == id);
+        }
+    }
+}
